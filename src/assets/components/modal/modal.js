f989e090c0eca87856f9934b265487ce012712db
Polina F <polina.fedo@mail.ru>
2022-12-01 13:00:53 +0700

    feat: add slider

--- src/assets/components/modal/modal.js
+++ src/assets/components/modal/modal.js
@@ -1,22 +1,42 @@
-import {Fancybox} from "@fancyapps/ui/src/Fancybox/index.js";
+import { Fancybox } from '@fancyapps/ui/src/Fancybox/index.js';
 
 const modalElement = document.querySelector('#callback-modal');
 const callbackButtons = document.querySelectorAll('.callback-btn');
 
-Fancybox.bind('#callback-modal]', {
+const modalSlidesElement = document.querySelector('#slides-callback');
+const callbackSlides = document.querySelectorAll('.slider__slide');
 
-});
+// Fancybox.bind('#callback-modal]', {
+//
+// });
 
 callbackButtons.forEach((element) => {
-  element.addEventListener('click', function (event) {
+  element.addEventListener('click', (event) => {
     if (event.target.dataset.title) {
       modalElement.querySelector('.modal__title').textContent = element.dataset.title;
     }
     Fancybox.show([{
       src: modalElement,
       type: 'html',
-    }])
-    console.log(event.target.dataset.title)
-  })
-})
+    }]);
+    console.log(event.target.dataset.title);
+  });
+});
 
+callbackSlides.forEach((element) => {
+  element.addEventListener('click', (event) => {
+    if (event.currentTarget.dataset.title) {
+      modalSlidesElement.querySelector('.modal__title').textContent = element.dataset.title;
+    }
+    if (event.currentTarget.dataset.img) {
+      console.log(modalSlidesElement.querySelector('.modal-slides__img'));
+      modalSlidesElement.querySelector('.modal-slides__img').src = element.dataset.img;
+    }
+    Fancybox.show([{
+      src: modalSlidesElement,
+      type: 'html',
+    }]);
+    console.log(event.currentTarget.dataset.img);
+    console.log(event.currentTarget.dataset.title);
+  });
+});
--- src/assets/components/slider/slider.js
+++ src/assets/components/slider/slider.js
@@ -1,1 +1,20 @@
-
+// const slider = document.querySelector('.slider');
+//
+// const sliderButtonPrev = slider.querySelector('.slider__prev-slide');
+// const sliderButtonNext = slider.querySelector('.slider__next-slide');
+// const slides = slider.querySelectorAll('.slider__slide');
+//
+// sliderButtonPrev.addEventListener('click', () => {
+//   slides.style.transform = 'translate3D(200px, 350px, 0px)';
+// });
+//
+// sliderButtonNext.addEventListener('click', () => {
+//   slides.style.transform = 'translate3D(0px, 0px, 0px)';
+// });
+$(function() {
+  $('.rotating-slider').rotatingSlider({
+    slideHeight: 360,
+    draggable: false,
+    slideWidth: Math.min(400, document.querySelector('.rotating-slider-container' ).offsetWidth)
+  });
+});
