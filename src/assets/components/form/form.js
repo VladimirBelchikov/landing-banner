ff72216198fd9e182897da77354bc771acb6e2b1
Polina F <polina.fedo@mail.ru>
2022-12-23 17:38:36 +0700

    fix: video block + modal slides

--- src/assets/components/form/form.js
+++ src/assets/components/form/form.js
@@ -1,10 +1,10 @@
-const messenger = document.getElementById('messenger');
-messenger?.addEventListener('change', (e) => {
-  // eslint-disable-next-line no-restricted-syntax
-  for (const el of messenger.children) {
-    el.classList.remove('form__messenger-label_active');
-  }
-  if (e.target.checked) {
-    e.target.labels[0].classList.add('form__messenger-label_active');
-  }
-});
+// const messenger = document.getElementById('messenger');
+// messenger?.addEventListener('change', (e) => {
+//   // eslint-disable-next-line no-restricted-syntax
+//   for (const el of messenger.children) {
+//     el.classList.remove('form__messenger-label_active');
+//   }
+//   if (e.target.checked) {
+//     e.target.labels[0].classList.add('form__messenger-label_active');
+//   }
+// });
